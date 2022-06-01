62df6ee3b26ed01d8f5014f8e88b2ab02d973633
Samigirum <samiabge23@gmail.com>
2022-06-01 18:09:02 +0300

    py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,18 +1,10 @@
+#!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number} is postive")
+    print(f"{number} is postive\n")
 elif number == 0:
-    print(f"{number} is zero")
+    print(f"{number} is zero\n")
 else:
-    print(f"{number} is negative")[A[A[A[A[A[A[A[A
-
-[A#!/usr/bin/python3
-[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[C[B[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cimport random
-number = random.randint(-10, 10)
-if number > 0:
-print(f"{number} is positive")
-elif number == 0:
-print(f"{number} is zero")
-else:
-print(f"{number} is negative")
+    print(f"{number} is negative\n")
+    
