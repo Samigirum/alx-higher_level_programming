ce95225adea7acc05ad19df07e9b114431df453d
Samigirum <samiabge23@gmail.com>
2022-06-02 04:47:23 +0300

    py

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import string
+
+for c in string.ascii_lowercase:
+print(char, end = ' ')[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[[C[C[C[C[C[C[C[C[C[C, end = ' ')
